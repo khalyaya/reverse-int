a582d38ef27cd5beeb2fc095c2dd69d6a49029b0
Liza Khaletskaya <khaletskayali@gmail.com>
2024-08-16 20:55:36 +0300

    feat: implement the function for reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n);
+  let nToString = n.toString();   
+  let i = nToString.length - 1;
+  let result = '';
+  while (i >= 0){
+    result = result + nToString[i];
+    i = i - 1; 
+  }
+  return parseInt(result);
 }
